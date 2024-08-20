eb3ded76bc43304ee951989ea3e76067ae79ebac
HolbieEnjoyer <8698@holbertonstudents.com>
2024-08-20 16:34:29 +0400

    Next task

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 """
-	COmmenting
+This script defines a function that safely returns the first element of a sequence.
+If the sequence is empty, the function returns None.
 """
 
 from typing import Sequence, Optional, Any
 
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
     """
-    Returns the first element of the sequence if it exists, otherwise returns None.
+    Return the first element of the sequence if it exists, otherwise return None.
 
     Parameters:
-    lst (Sequence[Any]): A sequence of elements of any type.
+        lst (Sequence[Any]): A sequence of elements of any type.
 
     Returns:
-    Optional[Any]: The first element of the sequence or None if the sequence is empty.
+        Optional[Any]: The first element of the sequence, or None if the sequence is empty.
     """
     if lst:
-        return lst[0]  # Return the first element if the list is not empty
-    else:
-        return None  # Return None if the list is empty
+        return lst[0]  # Return the first element if the sequence is not empty
+    return None  # Return None if the sequence is empty
